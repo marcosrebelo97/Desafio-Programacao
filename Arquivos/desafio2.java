5f091188a063b3f108d0282597dc81cf03316e13
Marcos Guerreiro Rebelo <37541973+marcosrebelo97@users.noreply.github.com>
2022-02-18 02:41:47 -0400

    Add files via upload

--- Arquivos/desafio2.java
+++ Arquivos/desafio2.java
@@ -1,49 +1,64 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-        
-        System.out.print("Digite sua senha: ");
-        Scanner scanner = new Scanner(System.in);
-        String password = scanner.next();
-        
-        int var_digito = 0;
-        int var_min = 0;
-        int var_max = 0;
-        int var_especial = 0;
-        
-        if (password.length() < 6) {
-            int minDigito = 6 - password.length();
-            System.out.printf("Adicione mais %d caracteres.\n", minDigito);
-        }
-        
-        for(int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
-            if(Character.isUpperCase(c)) {
-                var_max += 1;
-            }
-            else if (Character.isLowerCase(c)) {
-                var_min += 1;
-            }
-            else if (Character.isDigit(c)) {
-                var_digito += 1;
-            }
-            else if (password.contains("!") || password.contains("@") || password.contains("#") || password.contains("$") || 
-                password.contains("%") || password.contains("^") || password.contains("&") || password.contains("*") || 
-                password.contains("(") || password.contains(")") || password.contains("-") || password.contains("+")) {
-                var_especial +=1;
-            }
-            
-        }
-        
-        if (var_max == 0 || var_min == 0 || var_digito == 0 || var_especial == 0) {
-            System.out.println("Sua senha ainda não é segura! \nÉ necessário conter 6 caracteres entre:\n - Letras Maiúsculas e Minúsculas\n - Números e Caracteres Especiais !@#$%^&*()-+ ");
-        }
-        else {
-            System.out.println("Sua senha está segura!");
-        }
-        
-        scanner.close();
-    }
+  public static void main (String[]args)
+  {
+
+    System.out.print ("Digite sua senha: ");
+    Scanner scanner = new Scanner (System.in);
+    String password = scanner.next ();
+
+    int var_digito = 0;
+    int var_min = 0;
+    int var_max = 0;
+    int var_especial = 0;
+
+    if (password.length () < 6)
+      {
+	int minDigito = 6 - password.length ();
+	  System.out.printf ("Adicione mais %d caracteres.\n", minDigito);
+      }
+
+    for (int i = 0; i < password.length (); i++)
+      {
+	char c = password.charAt (i);
+	if (Character.isUpperCase (c))
+	  {
+	    var_max += 1;
+	  }
+	else if (Character.isLowerCase (c))
+	  {
+	    var_min += 1;
+	  }
+	else if (Character.isDigit (c))
+	  {
+	    var_digito += 1;
+	  }
+	else if (password.contains ("!") || password.contains ("@")
+		 || password.contains ("#") || password.contains ("$")
+		 || password.contains ("%") || password.contains ("^")
+		 || password.contains ("&") || password.contains ("*")
+		 || password.contains ("(") || password.contains (")")
+		 || password.contains ("-") || password.contains ("+"))
+	  {
+	    var_especial += 1;
+	  }
+
+      }
+
+    if (var_max == 0 || var_min == 0 || var_digito == 0 || var_especial == 0)
+      {
+	System.out.println
+	  ("Sua senha ainda nC#o C) segura! \nC	 necessC!rio conter 6 caracteres entre:\n - Letras MaiC:sculas e MinC:sculas\n - NC:meros e Caracteres Especiais !@#$%^&*()-+ ");
+      }
+    else
+      {
+	System.out.println ("Sua senha estC! segura!");
+      }
+
+    scanner.close ();
+  }
 }
+

16b742141a047d944eab88e144a67fdc93028f45
Marcos Guerreiro Rebelo <37541973+marcosrebelo97@users.noreply.github.com>
2022-02-18 02:41:28 -0400

    Add files via upload

--- Arquivos/desafio1.java
+++ Arquivos/desafio1.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args){
+  public static void main (String[]args)
+  {
 
-		Scanner s = new Scanner(System.in);
-		int q = 0;
-		
-		System.out.print("n = ");
-		q = s.nextInt();
+    Scanner s = new Scanner (System.in);
+    int q = 0;
 
-		for(int i=0, k=1; i < q; ++i, ++k){
-			for(int j=0; j < k; ++j)
-				System.out.print("*");
-			System.out.println("");
-		}
-	}
+      System.out.print ("n = ");
+      q = s.nextInt ();
+
+    for (int i = 0, k = 1; i < q; ++i, ++k)
+      {
+	for (int j = 0; j < k; ++j)
+	  System.out.print ("*");
+	System.out.println ("");
+      }
+  }
 }
